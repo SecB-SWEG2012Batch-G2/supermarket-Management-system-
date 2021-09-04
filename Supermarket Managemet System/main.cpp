3e7d86764433f6090dc854592a6b88ba42f6d8ad
BrookFeleke <felekebrook05@gmail.com>
2021-09-04 22:59:56 +0300

    Initialised sample data using Product structure

--- Supermarket Managemet System/main.cpp
+++ Supermarket Managemet System/main.cpp
@@ -1,89 +1,69 @@
 #include <iostream>
 using namespace std;
+
 struct Date{
     int DD, MM, YY;
 };
-struct Price {
 
-};
-//Bread_and_Bakery
-struct Bread_and_Bakery {
-int Serial_number;
-string type;
-string name;
-Date Production_Date;
-Date Expiration_date;
-float cost;
-float price;
-float Quantity;
-};
-//Pasta_and_Rice
-struct Pasta_and_Rice{
-int Serial_number;
-string name;
-string type;
-Date Expiration_date;
-Date Production_Date;
-float cost;
-float price;
-float Quantity;
-};
-//Dairy_and_Meat
-struct Dairy_and_Meat{
-int Serial_number;
-string name;
-string type;
-Date Expiration_date;
-Date Production_Date;
-float cost;
-float Price;
-float Quantity;
-};
+
+struct Product{
+    int ProductNumber;
+    string ProductName;
+    string Category;
+    string ProductType;
+    Date ProductionDate;
+    Date ExpireDate;
+    float Quantity;
+    string MeasurementUnit;
+    float UnitPrice;
+    float Rating;
+    int Sales=0;
+}Products[50];
 
 int main(){
+Product Products[50];
+Products[i] = {ProductNumber ,"ProductName ", " Category", "ProductType ",{date}, {edate},Quantity ,"MeasurementUnit ", ,Rating };
 
-//Sample Data for Bread and Bakery section
-Bread_and_Bakery Bread[8],Cookies[3];
-Bread[0] = {,"Bread"," Banana Bread",{3, 7, 2021},{6, 7, 2021}, 20,24,};
-Bread[1] = {,"Bread","Whole Wheat",{3, 7, 2021},{6, 7, 2021},10,12,};
-Bread[2] = {,"Bread","Sourdough",{3, 7, 2021},{6, 7, 2021},1,2,};
-Bread[3] = {,"Bread","Baguette",{3, 7, 2021},{6, 7, 2021},15,2,};
-Bread[4] = {,"Bread","Difo Dabo ",{3, 7, 2021},{6, 7, 2021},30,40,};
-Bread[5] = {,"Bread","White Bread",{3, 7, 2021},{6, 7, 2021},2,3,};
-Bread[6] = {,"Bread","Carrot cake",{3, 7, 2021},{6, 7, 2021}, , ,};
-Bread[7] = {,"Bread","Bagels",{3, 7, 2021},{6, 7, 2021}, , ,};
-Cookies[0] = {,"Cookie","Chocolate Chip",{3, 7, 2021},{6, 7, 2021}, , ,};
-Cookies[1] = {,"Cookie","Shortbread Cookie",{3, 7, 2021},{6, 7, 2021}, , ,};
-Cookies[2] = {,"Cookie","Peanut Butter Cookie",{3, 7, 2021},{3, 7, 2021}, , ,};
+Products[i] = { ," ", " ", " ",{date}, {edate}, ," ", , };
 
+//Initializing Bread and Bakery sample data
+Products[1] = {1 ,"Banana Bread ", "Bread and Bakery ", "Bread ",{date}, {edate}, 200,"Loafes ", ,4.9 };
+Products[2] = {2 ,"Whole Wheat ", "Bread and Bakery ", "Bread ",{date}, {edate},200 ,"Loafes ", ,2.5 };
+Products[3] = {3 ,"Sourdough ", "Bread and Bakery ", "Bread ",{date}, {edate}, 200,"Loafes ", ,4.7 };
+Products[4] = {4 ,"Baguette ", "Bread and Bakery ", "Bread ",{date}, {edate},200 ,"Loafes ", ,3 };
+Products[5] = {5 ,"Difo Dabo ", "Bread and Bakery ", "Bread ",{date}, {edate}, 200," Loafes", ,4.9 };
+Products[6] = {6 ,"White Bread ", "Bread and Bakery ", "Bread ",{date}, {edate}, 200,"Loafes ", ,2.8 };
+Products[7] = {7 ,"Carrot Cake ", "Bread and Bakery ", "Bread ",{date}, {edate},200 ,"Loafes ", ,4.1 };
+Products[8] = {8 ,"Bagels ", "Bread and Bakery ", "Bread ",{date}, {edate}, 200," Loafes", ,3.9 };
+Products[9] = {9 ,"Chocolate Chip ", "Bread and Bakery ", "Cookies ",{date}, {edate},200 ,"Killograms ", ,4.8 };
+Products[10] = {10 ,"Shortbead Cookie ", "Bread and Bakery ", "Cookies ",{date}, {edate}, 200,"Killograms ", ,4 };
+Products[11] = {11 ,"Peanut Butter200 ", "Bread and Bakery ", "Cookies ",{date}, {edate},200 ,"Killograms ", ,2.5 };
 
-//Sample Data for Pasta and Rice
-Pasta_and_Rice Pasta[6], Rice[2];
-Pasta[0] = {,"Pasta","Short Pasta",{3, 7, 2021},{6, 7, 2023}, , ,};
-Pasta[1] = {,"Pasta","Long Pasta",{3, 7, 2021},{6, 7, 2023}, , ,};
-Pasta[2] = {,"Pasta","Macaroni",{3, 7, 2021},{6, 7, 2023}, , ,};
-Pasta[3] = {,"Pasta","Fettuccine",{3, 7, 2021},{6, 7, 2023}, , ,};
-Pasta[4] = {,"Pasta","Lasagna",{3, 7, 2021},{6, 7, 2023}, , ,};
-Pasta[5] = {,"Pasta","Spaghetti",{3, 7, 2021},{3, 7, 2023}, , ,};
-Rice[0] = {,"Rice","Brown rice",{3, 7, 2021},{6, 7, 2023}, , ,};
-Rice[1] = {,"Rice","White Rice",{3, 7, 2021},{6, 7, 2023}, , , }
-//Sample Data for Meat and Dairy Products
-Dairy_and_Meat Meat[5], Dairy[6];
-Meat[0] = {,"Meat","Beef",{3, 7, 2021},{6, 7, 2021}, , ,};
-Meat[1] = {,"Meat","Pork",{3, 7, 2021},{6, 7, 2021}, , ,};
-Meat[2] = {,"Meat","Mutton",{3, 7, 2021},{6, 7, 2021}, , ,};
-Meat[3] = {,"Meat","Chicken",{3, 7, 2021},{6, 7, 2021}, , ,};
-Meat[4] = {,"Meat","Turkey",{3, 7, 2021},{6, 7, 2021}, , ,};
+//Initialising Pasta and rice sample data
+Products[12] = {12,"Short Pasta ", "Pasta and Rice ", "Pasta ",{date}, {edate}, 100,"Killograms ", ,4.3 };
+Products[13] = {13,"Long Pasta ", "Pasta and Rice ", "Pasta ",{date}, {edate}, 100,"Killograms ", ,4.3 };
+Products[14] = {14,"Macaroni ", "Pasta and Rice ", "Pasta ",{date}, {edate}, 100,"Killograms ", ,4 };
+Products[15] = {15,"Fettuccine", "Pasta and Rice ", "Pasta ",{date}, {edate}, ,"Killograms ", ,2.5 };
+Products[16] = {16,"Lasagna", "Pasta and Rice ", "Pasta ",{date}, {edate}, 100,"Killograms ", ,5 };
+Products[17] = {17,"Spaghetti", "Pasta and Rice ", "Pasta ",{date}, {edate}, 100," Killograms", ,4.9 };
+Products[18] = {18,"Brown Rice ", "Pasta and Rice ", "Rice ",{date}, {edate}, 100,"Killograms ", ,4.7 };
+Products[19] = {19,"White Rice ", "Pasta and Rice ", "Rice ",{date}, {edate},100 ," Killograms", ,2.5 };
 
-Dairy[0] = {,"Dairy","Butter",{},{6, 7, 2021}, , ,};
-Dairy[1] = {,"Dairy","Milk",{},{6, 7, 2021}, , ,};
-Dairy[2] = {,"Dairy","Cheese",{},{6, 7, 2021}, , ,};
-Dairy[3] = {,"Dairy","Yoghurt",{},{6, 7, 2021}, , ,};
-Dairy[4] = {,"Dairy","Milk Powder",{},{6, 7, 2021}, , ,};
-Dairy[5] = {,"Dairy","Ice Cream",{},{6, 7, 2021}, , ,};
+//Initialising Dairy and meat sample data
+Products[20] = {20 ,"Beef ", "Dairy and Meat ", "Meat ",{date}, {edate},100 ,"Killogram ", ,4.9 };
+Products[21] = {21 ,"Pork ", "Dairy and Meat ", "Meat ",{date}, {edate},100 ,"Killogram ", ,2.5 };
+Products[22] = {22 ,"Mutton ", "Dairy and Meat ", "Meat ",{date}, {edate},100 ,"Killogram ", , 4.4};
+Products[23] = {23 ,"Chicken ", "Dairy and Meat ", "Meat ",{date}, {edate},100 ,"Killogram ", ,4.9 };
+Products[24] = {24 ,"Turkey", "Dairy and Meat ", "Meat ",{date}, {edate},100 ,"Killogram ", ,3.9 };
+Products[25] = {25 ,"Butter ", "Dairy and Meat ", "Dairy ",{date}, {edate}, ,"Killograms ", ,5 };
+Products[26] = {26 ,"Milk ", "Dairy and Meat ", "Dairy ",{date}, {edate}, 100,"Liters ", ,4.9 };
+Products[27] = {27 ,"Mozzarella ", "Dairy and Meat ", "Cheese ",{date}, {edate},100 ,"Killogram ", ,4.5 };
+Products[27] = {27 ,"Cheddar ", "Dairy and Meat ", "Cheese ",{date}, {edate},100 ,"Killogram ", ,4.9 };
+Products[27] = {27 ,"Parmesan ", "Dairy and Meat ", "Cheese ",{date}, {edate},100 ,"Killogram ", ,4.4 };
+Products[27] = {27 ,"Gouda ", "Dairy and Meat ", "Cheese ",{date}, {edate},100 ,"Killogram ", ,5 };
+Products[28] = {28 ,"Yoghurt ", "Dairy and Meat ", "Dairy ",{date}, {edate},100 ,"Litres ", ,4.9 };
+Products[29] = {29 ,"Powdered Milk ", "Dairy and Meat ", "Dairy ",{date}, {edate},100 ,"Killogram ", ,4.6 };
+Products[30] = {30 ,"Ice Cream", "Dairy and Meat ", "Dairy ",{date}, {edate},100 ,"Killogram ", ,5 };
 
-
-return 0;
 
 }
-
