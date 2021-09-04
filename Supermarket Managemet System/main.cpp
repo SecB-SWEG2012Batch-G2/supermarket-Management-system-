9f1df911ab4f4331fb160bf67321f7c171c0e5f0
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2021-09-04 15:06:57 +0300

    final days
    project finalization start

--- Supermarket Managemet System/main.cpp
+++ Supermarket Managemet System/main.cpp
@@ -2,8 +2,44 @@
 
 using namespace std;
 
+/*
+    Kitchen Utensils
+    Sanitary Products
+    Health and Body Care
+*/
+
+struct Date{
+    int dd,mm,yy;
+};
+
+struct KitchenUtensils{
+    int serialNumber;
+    string name;
+    string type;
+};
+
+struct SanitaryProducts{
+    int serialNumber;
+    string name;
+    string manufacturer;
+    Date productionDate;
+    Date expireDate;
+    string type;
+};
+
+struct HealthAndBodyCare{
+    int serialNumber;
+    string name;
+    string manufacturer;
+    Date productionDate;
+    Date expireDate;
+    string type;
+};
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    /* PROJECT START */
+    cout << "HELLO" << endl;
     return 0;
 }
